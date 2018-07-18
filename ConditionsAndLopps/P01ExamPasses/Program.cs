355eb837c30112b40eec35e1a19c3b79c64125e1
GeorgiMM <georgi_metodiev_2@abv.bg>
2018-07-18 18:02:26 +0300

    P01

---
+++ ConditionsAndLopps/P01ExamPasses/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace P01ExamPasses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
